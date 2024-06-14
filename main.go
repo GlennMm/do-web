6881cbe546113834891349d09b9ba6a53798a536
GlennMm <matsiweglenn@gmail.com>
2024-06-14 05:02:10 +0200

    hello sqlc

--- main.go
+++ main.go
@@ -11,16 +11,17 @@ func main() {
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 
   router := http.NewServeMux()
+  port := 8000
 
   router.HandleFunc("GET /{$}", func(w http.ResponseWriter, r *http.Request) {
     w.Header().Set("Content-Type", "text/html; charset utf-8")
     w.Write([]byte("<h1>Hello World</h1>"))
   })
 
-  logger.Info("HTTP server is running", "port", 8080)
+  logger.Info("HTTP server is running", "port", port)
  
   srv := http.Server{
-    Addr: ":8080",
+    Addr: ":8000",
     Handler: router,
   }
 
